512bb9a1f4ef2355633258f6f6b59049d56cf253
Stas9700 <chudenkov97@mail.ru>
2020-10-06 22:37:56 +0300

    Initial commit

---
+++ ModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Library
+{
+    public abstract class ModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+        /*
+         * Класс который является базовым для всех в папках Model и ViewModel.
+         * Просто реализует интерфейс PropertyChange для удобства
+         */
+    }
+}
---
+++ RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace MVVM_Library
+{
+    public class RelayCommand : ICommand
+    {
+        /*
+         * Стырил с форума, реализайция интерфейса ICommand. Если честно, до конца хз как работает. Но завязанно на делегатах :)
+         */
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
